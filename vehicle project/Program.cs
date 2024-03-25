ab558ebf74b7f7eb4d492e996593f91183fa899c
108815 <108815@mgt.sjp.ac.lk>
2024-03-25 07:48:27 +0530

    updated branch

---
+++ vehicle project/Program.cs
@@ -0,0 +1,15 @@
+﻿using System.Net.Http.Headers;
+using System.Numerics;
+
+namespace vehicle_project
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            
+        }
+        
+        
+    }
+}
---
+++ vehicle project/bike.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace vehicle_project
+{
+    internal class bike
+    {
+        public string id { get; set; }
+        public  string model  { get; set; }
+        public  int year  { get; set; }
+
+
+        public void starting()
+        {
+            Console.WriteLine("engination is started");
+        }
+        public void run()
+        {
+            Console.WriteLine("Bike is Now forwarding");
+        }
+    }
+}
